8580a5e7174925743f5bf297d62ade98282c6d58
Greg Knoll <gregory@bccn-berlin.de>
2022-10-06 08:32:04 -0700

    REF: clean up kilosort script...
    ...add docstring at the beginning
       clearly demarcate blocks, separating paths, options, Kilosort calls,
       save, etc.

--- main_kilosort_25_Torben.m
+++ main_kilosort_25_Torben.m
@@ -1,28 +1,56 @@
-%% you need to change most of the paths in this block
+%{
+This file is the main preprocessing step to isolate
+single unit recordings from Neuropixels using the output from Trodes
 
-delete_previous_KS_run = true; %WARNING deletes all files in data folder except for Trodes-specific files
+It runs the four main spike-sorting/clustering steps of Kilosort 2.5
+1) Data preprocessing and channel selection
+2) Drift calculation and batch reordering
+3) The main optimization: clustering and template matching
+4) Final merges and splits, then threshold detection
+
+author: TO Jan-Dec 2021
+Updated by GK: Okt 6, 2022
+%}
+
+
+% ---------------------------------------------------------------------- %
+%       Change most of the paths in this block prior to execution
+% ---------------------------------------------------------------------- %
+%WARNING deletes all files in data folder except for Trodes-specific files
+delete_previous_KS_run = false;
+remove_duplicates = false;
 
 probenum = 2;
 
 docs_path = 'C:\Users\science person\Documents\';
-preprocessing_script_path = strcat(docs_path, 'MATLAB\neuropixels_preprocessing\');
-sortingQuality_path = strcat(preprocessing_script_path, 'sortingQuality');
+preprocessing_path = strcat(docs_path,'MATLAB\neuropixels_preprocessing\');
+sortingQuality_path = strcat(preprocessing_path, 'sortingQuality');
 
 addpath(sortingQuality_path)
 addpath(strcat(sortingQuality_path, '\core'))
 addpath(strcat(sortingQuality_path, '\helpers'))
 
-addpath(genpath(strcat(docs_path, 'MATLAB\Kilosort-2.5'))) % path to kilosort folder
-addpath(strcat(docs_path, 'npy-matlab-master')) % for converting to Phy, https://github.com/kwikteam/npy-matlab
+% path to kilosort folder
+addpath(genpath(strcat(docs_path, 'MATLAB\Kilosort-2.5'))) 
 
-rootZ = 'X:\NeuroData\Nina2\20210623_121426.rec'; % the raw data binary file is in this folder CANT HAVE TRAILING SLASH
-rootH = 'X:\NeuroData\Nina2\20210623_121426.rec'; % path to temporary binary file (same size as data, should be on fast SSD)
+% for converting to Phy, https://github.com/kwikteam/npy-matlab
+addpath(strcat(docs_path, 'npy-matlab-master')) 
 
-pathToYourConfigFile = strcat(preprocessing_script_path, 'configFiles'); % take from Github folder and put it somewhere else (together with the master_file)
-% chanMapFile = 'neuropixPhase3B1_kilosortChanMapTORBEN.mat';
-chanMapFile = 'channelMap.mat';
+% the raw data binary file is in this folder CANT HAVE TRAILING SLASH
+rootZ = 'X:\NeuroData\Nina2\20210623_121426.rec'; 
+% path to temporary binary file (same size as data, should be on fast SSD)
+rootH = 'X:\NeuroData\Nina2\20210623_121426.rec'; 
+% ---------------------------------------------------------------------- %
+
+
+% ---------------------------------------------------------------------- %
+%               Setup and Load Kilosort Config File
+% ---------------------------------------------------------------------- %
+% take from Github folder and put it somewhere else (with master_file)
+pathToYourConfigFile = strcat(preprocessing_path, 'configFiles'); 
 
 %make chan map 
+chanMapFile = 'channelMap.mat';
 getChanMap(rootZ); %spikegadgets tool
 
 %kilosort subfolder for KS output
@@ -34,6 +62,9 @@ if ~isfolder(fullfile(rootZ,kfolder)),mkdir(fullfile(rootZ,kfolder)), end
 %kilosort subfolder containing Trodes binary data file (Trodes will make a
 %subfolder when exporting binary data file for kilosort)
 ksdatafolder = strcat(ss,'.kilosort');
+
+% binary dat file
+kfile = strcat(ss,'.probe',num2str(probenum),'.dat');
 
 %make config struct
 if delete_previous_KS_run
@@ -47,74 +78,98 @@ scriptpath = mfilename('fullpath'); scriptpath= [scriptpath,'.m'];
 ff = fullfile(rootZ,kfolder,scriptname);
 ops.main_kilosort_script = ff;
 copyfile(scriptpath,ff);
+% ---------------------------------------------------------------------- %
 
-%KS ops
+
+% ---------------------------------------------------------------------- %
+%                         Kilosort Parameters
+% ---------------------------------------------------------------------- %
+run(fullfile(pathToYourConfigFile, 'StandardConfig_384Kepecs.m'))
+
 ops.trange    = [0 Inf]; % time range to sort
 ops.NchanTOT  = 384; % total number of channels in your recording
 
-run(fullfile(pathToYourConfigFile, 'StandardConfig_384Kepecs.m'))
-ops.fproc   = fullfile(rootH, kfolder, 'temp_wh.dat'); % proc file on a fast SSD
+% proc file on a fast SSD
+ops.fproc   = fullfile(rootH, kfolder, 'temp_wh.dat'); 
 ops.chanMap = fullfile(rootZ, chanMapFile);
-
-%% this block runs all the steps of the algorithm
 
 % main parameter changes from Kilosort2 to v2.5
 ops.sig        = 20;  % spatial smoothness constant for registration
 ops.fshigh     = 300; % high-pass more aggresively
-ops.nblocks    = 1; % blocks for registration. 0 turns it off, 1 does rigid registration. Replaces "datashift" option. 
-%jesus remember to remove this amy, trying a test for torben!
 
+% Blocks for registration (Replaces "datashift" option)
+%   0 turns it off
+%   1 does rigid registration
+ops.nblocks    = 1; 
 
-% binary dat file
-kfile = strcat(ss,'.probe',num2str(probenum),'.dat');
 ops.fbinary = fullfile(rootZ, ksdatafolder, kfile);
-%ops.fbinary = 'E:\Neurodata\20210107_172745_20210108_164001\combined.dat';
+% ---------------------------------------------------------------------- %
 
-% preprocess data to create temp_wh.dat
+
+% ---------------------------------------------------------------------- %
+%                      Run Kilosort Algorithm
+% ---------------------------------------------------------------------- %
+% 1) preprocess data to create temp_wh.dat
 rez = preprocessDataSub(ops);
 
 % NEW STEP TO DO DATA REGISTRATION
 % last input is for shifting data
 rez = datashift2(rez, 1);
+
 % ORDER OF BATCHES IS NOW RANDOM, controlled by random number generator
 iseed = 1;
                  
-% main tracking and template matching algorithm
+% 2) and 3) main tracking and template matching algorithm
 rez = learnAndSolve8b(rez, iseed);
 % check_rez(rez);
 
-% OPTIONAL: remove double-counted spikes - solves issue in which individual spikes are assigned to multiple templates.
+% OPTIONAL: remove double-counted spikes - solves issue in which 
+% individual spikes are assigned to multiple templates.
 % See issue 29: https://github.com/MouseLand/Kilosort/issues/29
-%rez = remove_ks2_duplicate_spikes(rez);
+if remove_duplicates
+    rez = remove_ks2_duplicate_spikes(rez);
+end
 
-% final merges
+% 4a) final merges
 rez = find_merges(rez, 1);
 % check_rez(rez);
 
-% final splits by SVD
+% 4b) final splits by SVD
 rez = splitAllClusters(rez, 1);
 % check_rez(rez);
 
-% decide on cutoff
+% 4c) decide on cutoff
 rez = set_cutoff(rez);
 % check_rez(rez);
 
-% eliminate widely spread waveforms (likely noise)
+% 4d) eliminate widely spread waveforms (likely noise)
 rez.good = get_good_units(rez);
+% ---------------------------------------------------------------------- %
 
+
+% ---------------------------------------------------------------------- %
+%                       Report and Save Results
+% ---------------------------------------------------------------------- %
 fprintf('found %d good units \n', sum(rez.good>0))
 
-% write to Phy
-fprintf('Saving results to Phy  \n')
+fprintf('Saving results to Phy \n')
 rezToPhy(rez, fullfile(rootH,kfolder));
+% ---------------------------------------------------------------------- %
 
-% compute quality metrics
+
+% ---------------------------------------------------------------------- %
+%                       Compute Quality Metrics
+% ---------------------------------------------------------------------- %
 [cids, uQ, cR, isiV, histC] = sqKilosort.computeAllMeasures(fullfile(rootH, kfolder));
 
-sqKilosort.metricsToPhy(fullfile(rootH, kfolder), cids, uQ, isiV, cR, histC);
 %save them for phy
+sqKilosort.metricsToPhy(fullfile(rootH, kfolder), cids, uQ, isiV, cR, histC);
+% ---------------------------------------------------------------------- %
 
-%% if you want to save the results to a Matlab file...
+
+% ---------------------------------------------------------------------- %
+%               Save the results to a Matlab file
+% ---------------------------------------------------------------------- %
 
 % discard features in final rez file (too slow to save)
 rez.cProj = [];
