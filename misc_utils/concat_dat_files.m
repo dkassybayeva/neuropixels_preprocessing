72ba88d36f4c57e1d55388f8c1bc40fce223d821
Greg Knoll <gregory@bccn-berlin.de>
2023-03-07 19:11:26 +0100

    CLN: the raw data concatenation function...
    ...code was kind of a mess.  I added comments, removed
       commented out code, reorganized (pulling some unchanging
       variables out of loops, etc), and added a progress print.

--- misc_utils/concat_dat_files.m
+++ misc_utils/concat_dat_files.m
@@ -1,73 +1,82 @@
-function [ops] =  concat_dat_files(ops, fbinary, fproc, stub)
-% fbinary: cell list of paths to Trodes recording session FOLDERS (will assume .kilosort subfolder with extracted binary raw data file)
-% fproc: path to merged output binary data FILE
+function [ops] = concat_dat_files(ops, probe_dat_list, fproc, stub)
+%{ 
+    probe_dat_list: cell list of paths to the probe.dat files exported by 
+    Trodes in the .rec/.kilosort subfolder
 
-% fbinary = {'D:\Neurodata\TQ01\20210107_172745\20210107_172745.kilosort\20210107_172745.probe1.dat', 'D:\Neurodata\TQ01\20210108_164001\20210108_164001.kilosort\20210108_164001.probe1.dat'};
-% fproc = 'E:\Neurodata\20210107_172745_20210108_164001\combined.dat';
+    fproc: path to merged output binary data FILE
+    e.g., combined.mat
 
-NchanTOT = 384;
+    edited by GK, March 6th, 2023
+%}
 
-fidW = fopen(fproc,   'a+'); % open for writing processed data   
+% ----- open the output processed data file for writing ----- %
+fidW = fopen(fproc, 'a+');  
 if fidW<3
-    error('Could not open %s for writing.',ops.fproc);    
+    error('Could not open %s for writing.', fproc);    
 end
-tic;
-file_lens = [];
-for i = 1:length(fbinary)
-    fprintf('Time %3.0fs. concatenating file... \n', toc);
-    %path to file
-    [~,ss] = fileparts(fbinary{i});
-    ksdatafolder = strcat(ss,'.kilosort');
-    kfile = strcat(ss,'.probe2.dat');
-    datafile = fullfile(fbinary{i}, ksdatafolder, kfile);
-    
-    bytes       = get_file_size(datafile); % size in bytes of raw binary
-    NT       = ops.NT ; % number of timepoints per batch
-    NchanTOT = ops.NchanTOT; % total number of channels in the raw binary file, including dead, auxiliary etc
+% ----------------------------------------------------------- %
 
+NchanTOT = ops.NchanTOT;    % total number of channels in the raw binary file, including dead, auxiliary etc
+NT = ops.NT;          % number of timepoints per batch
+% NT = 40*(65600*2*NchanTOT); GK_note: I have no idea what this means
+stub_bytes = NT*100*2*NchanTOT;
+
+ops.tstart  = ceil(ops.trange(1) * ops.fs); % starting timepoint for processing data segment
+
+
+
+% file_lens = [];  % GK_note: doesn't seem to be used
+tic;
+for session_i = 1:length(probe_dat_list)
+    fprintf('Time %3.0fs. Concatenating file... \n', toc);
+
+    datafile = probe_dat_list{session_i};
     
     if stub
-         %only making small snippet of file, should be 60 batches!
-        %NT = 40*(65600*2*NchanTOT);
-        bytes = NT*100*2*NchanTOT;
+        %only making small snippet of file, should be 60 batches! 
+        bytes = stub_bytes;
+    else
+        bytes = get_file_size(datafile);  % size in bytes of raw binary
     end
     
-    nTimepoints = floor(bytes/NchanTOT/2); % number of total timepoints
-    ops.tstart  = ceil(ops.trange(1) * ops.fs); % starting timepoint for processing data segment
-    ops.tend    = min(nTimepoints, ceil(ops.trange(2) * ops.fs)); % ending timepoint
-    ops.sampsToRead = ops.tend-ops.tstart; % total number of samples to read
-    Nbatch      = ceil(ops.sampsToRead /NT);
+    % These ops params may change depending on the number of bytes
+    nTimepoints = floor(bytes/NchanTOT/2);  % number of total timepoints
+    ops.tend = min(nTimepoints, ceil(ops.trange(2) * ops.fs));  % ending timepoint
+    ops.sampsToRead = ops.tend - ops.tstart;  % total number of samples to read
+
+%     Nbatch = ceil(ops.sampsToRead /NT);  % GK_note: doesn't seem to be used
 
     %if i == 1
     %    ops.midpoint = Nbatch;
     %end
-    file_lens = [file_lens, bytes];
+    
+%     file_lens = [file_lens, bytes];  % GK_note: doesn't seem to be used
+
     %nTimepoints = nTimepoints + floor(bytes/NchanTOT/2); % number of total timepoints
     
-    %ok we have to do something about this... woudl these parameters be per
-    %file?
-    %50 == reading larger chunks!
-   % NT = 40*(65600*2*NchanTOT); % 2seconds * 30khz * 2bytes
-   % Nbatch = ceil(bytes / 2*NchanTOT*NT);
+    % ok we have to do something about this... 
+    % would these parameters be per file?
+    % 50 == reading larger chunks!
+    % NT = 40*(65600*2*NchanTOT); % 2seconds * 30khz * 2bytes
+    % Nbatch = ceil(bytes / 2*NchanTOT*NT);
     
-    fid         = fopen(datafile, 'r'); % open for reading raw data
+    fid = fopen(datafile, 'r');  % open for reading raw data
     if fid<3
-        error('Could not open %s for reading.',ops.fbinary);
+        error('Could not open %s for reading.', datafile);
     end
     
-   
-    
     c = 0;
-    for ind = 0:NT:bytes
-        
-        
-        offset = ind;
+    for offset = 0:NT:bytes
         fseek(fid, offset, 'bof');
+        if ~mod(offset / NT, 1e4)
+            disp(offset / bytes * 100)
+        end
+
         
         %if we're at the end of the file only read the up to the end of the
         %file!
-        if ind + NT >= bytes
-            toread = bytes - ind;
+        if offset + NT >= bytes
+            toread = bytes - offset;
         else
             toread = NT;
         end
@@ -82,7 +91,7 @@ for i = 1:length(fbinary)
         count = fwrite(fidW, buff, 'int16'); % write this batch to binary file
         c = c+ count;
         if count~=numel(buff)
-            error('Error writing batch %g to %s. Check available disk space.',ind,fproc);
+            error('Error writing batch %g to %s. Check available disk space.',offset,fproc);
         end
         
         
