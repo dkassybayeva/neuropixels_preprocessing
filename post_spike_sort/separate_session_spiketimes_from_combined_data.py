4c24d782c236fb82eed935f29f5c1f0354385f8f
Greg Knoll <gregory@bccn-berlin.de>
2023-03-10 13:11:30 +0100

    FIX: many mistakes in spike separation script.

--- post_spike_sort/separate_session_spiketimes_from_combined_data.py
+++ post_spike_sort/separate_session_spiketimes_from_combined_data.py
@@ -23,6 +23,9 @@ import h5py
 from joblib import load, dump
 from scipy.io.matlab import loadmat
 
+from neuropixels_preprocessing.misc_utils.TrodesToPython.readTrodesExtractedDataFile3 \
+    import get_Trodes_timestamps
+
 # ----------------------------------------------------------------------- #
 rat = 'Nina2'
 probe = 'probe1'
@@ -32,9 +35,9 @@ fs = 30e3  # sampling frequency in Hz
 
 
 # ----------------------------------------------------------------------- #
-combined_session = '20210616_20210618'
+combined_session = '20210623_20210625'
 
-combined_dir = path.join('X:/Neurodata', rat, combined_session, probe, KS_version) + '/'
+combined_dir = f'X:NeuroData/{rat}/{combined_session}/{probe}/{KS_version}/'
 
 """
 # Phy's clustering results have to be converted to mat file before
@@ -64,26 +67,26 @@ spike_times_arr = h5py.File(combined_dir + 'spike_times.mat')['spikeTimes'][()][
 
 # ----------------------------------------------------------------------- #
 session1 = '20210623_121426'
-session1_base_dir = path.join('D:/Neurodata', rat, sesion1 + '.rec')
+session1_base_dir = path.join('D:/Neurodata', rat, session1 + '.rec')
 
 session2 = '20210625_114657'
-session2_base_dir = path.join('D:/Neurodata', rat, sesion2 + '.rec')
+session2_base_dir = path.join('D:/Neurodata', rat, session2 + '.rec')
 
 # load Trodes timestamps
 timestamps_dat_1 = path.join(session1_base_dir, session1 + '.kilosort', session1 + '.timestamps.dat')
 trodes_timestamps_1 = get_Trodes_timestamps(timestamps_dat_1)
 last_sample_sesh1 = trodes_timestamps_1[-1]  # last_sample_sesh1 / fs = time in sec of the last recorded Trodes sample
-last_spike_sesh1_ms = int(np.ceil(last_spike_sesh1 / fs * 1000))
+last_spike_sesh1_ms = int(np.ceil(last_sample_sesh1 / fs * 1000))
 spike_mat_sesh1 = np.zeros((len(good_clusters), last_spike_sesh1_ms), dtype='uint8')
 
 timestamps_dat_2 = path.join(session2_base_dir, session2 + '.kilosort', session2 + '.timestamps.dat')
 trodes_timestamps_2 = get_Trodes_timestamps(timestamps_dat_2)
 last_sample_sesh2 = trodes_timestamps_2[-1]  # last_sample_sesh2 / fs = time in sec of the last recorded Trodes sample
-last_spike_sesh2_ms = int(np.ceil(last_spike_sesh2 / fs * 1000))
+last_spike_sesh2_ms = int(np.ceil(last_sample_sesh2 / fs * 1000))
 spike_mat_sesh2 = np.zeros((len(good_clusters), last_spike_sesh2_ms), dtype='uint8')
 # ----------------------------------------------------------------------- #
 
-combined_timestamps = np.concatenate(trodes_timestamps_1, trodes_timestamps_2 + last_sample_sesh1)
+combined_timestamps = np.concatenate((trodes_timestamps_1, trodes_timestamps_2 + last_sample_sesh1))
 
 print('Creating spike mat...')
 for i, clust_i in enumerate(good_clusters):
@@ -101,7 +104,7 @@ for i, clust_i in enumerate(good_clusters):
     # to clust_spike_sample_idx, but there are sometimes missing samples, such that
     # the sample numbers aren't always consecutive.
     # To be safe, index the Trodes samples in this way.
-    global_clust_spike_sample_idx = trodes_timestamps[clust_spike_sample_idx]
+    global_clust_spike_sample_idx = combined_timestamps[clust_spike_sample_idx]
 
     spike_samples_sesh1 = global_clust_spike_sample_idx[global_clust_spike_sample_idx <= last_sample_sesh1]
     spike_samples_sesh2 = global_clust_spike_sample_idx[global_clust_spike_sample_idx > last_sample_sesh1]
@@ -118,9 +121,9 @@ for i, clust_i in enumerate(good_clusters):
     spike_mat_sesh2[i, spiktime_ms_inds_sesh2] = 1
 
 results = {'spike_mat': spike_mat_sesh1, 'row_cluster_id': good_clusters}
-dump(results, output_dir + f'spike_mat_{session1}_in_ms.npy', compress=3)
+dump(results, combined_dir + f'spike_mat_{session1}_in_ms.npy', compress=3)
 
 results = {'spike_mat': spike_mat_sesh2, 'row_cluster_id': good_clusters}
-dump(results, output_dir + f'spike_mat_{session2}_in_ms.npy', compress=3)
-print('\nSaved to ' + output_dir)
+dump(results, combined_dir + f'spike_mat_{session2}_in_ms.npy', compress=3)
+print('\nSaved to ' + combined_dir)
 
