178e22c66279aafe98be922f9b6f5bed46208563
Dariya K <dariya.kassybayeva@nu.edu.kz>
2024-04-10 15:11:58 +0200

    behavior_dict modifications for DC
    Adjusted parameters for Detection Confidence behavior files to be aligned with spikes

--- post_spike_sort/post_cluster_pipeline_auditory_detectionConfidence.py
+++ post_spike_sort/post_cluster_pipeline_auditory_detectionConfidence.py
@@ -87,7 +87,7 @@ if SPIKES_AND_TTL:
   # %% Cut Recording into Auditory Tuning and Detection Confidence based on TTL
   
   
-# %%
+# %% behavior for Detection Confidence
 if BEHAVIOR:
     _sd = load(preprocess_dir + 'TrialEvents.npy')
     
@@ -97,44 +97,45 @@ if BEHAVIOR:
     
     trialstart_str = 'recorded_TTL_trial_start_time'
     trial_len = _sd[trialstart_str][1:] - _sd[trialstart_str][:-1]
-    TrialStartTimestamp = _sd['TrialStartTimestamp'][:n_trials]
-    trialstart_df = np.ediff1d(TrialStartTimestamp)
-    max_gap = max(trialstart_df)
-#     behav_dict = dict(
-#     volume = _sd['Custom']['Volume'][:n_trials],
-#     frequency = _sd['Custom']['Frequency'][:n_trials],
-#     TrialStartTimestamp = _sd['TrialStartTimestamp'][:n_trials],
-#     TrialEndTimestamp = _sd['TrialEndTimestamp'][:n_trials],
-#     recorded_TTL_trial_start_time = _sd[trialstart_str][:n_trials],
-#     trial_len = trial_len[:n_trials]
-#     )
-#     behav_df = pd.DataFrame.from_dict(behav_dict)
     
-#     dump(behav_df, preprocess_dir + "behav_df", compress=3)
+    #trialstart_df = np.ediff1d(TrialStartTimestamp)
+    #max_gap = max(trialstart_df)
+    behav_dict = dict(
+        stimulus_start_time = _sd['Custom']['TrialData']['StimulusStartTime'][:n_trials],
+        reward_start_time = _sd['Custom']['TrialData']['RewardStartTime'][:n_trials],
+        signal_volume = _sd['Custom']['TrialData']['SignalVolume'][:n_trials],
+        TrialStartTimestamp = _sd['TrialStartTimestamp'][:n_trials],
+        TrialEndTimestamp = _sd['TrialEndTimestamp'][:n_trials],
+        recorded_TTL_trial_start_time = _sd[trialstart_str][:n_trials],
+        trial_len = trial_len[:n_trials]
+    )
+    behav_df = pd.DataFrame.from_dict(behav_dict)
+    
+    dump(behav_df, preprocess_dir + "behav_df", compress=3)
 
-# downsample_dt = 25  # sample period in ms
-# # %%
-# #----------------------------------------------------------------------#
-# # Align the spikes with behavior
-# for probe_i in range(1, metadata['n_probes']+1):
-#     metadata['probe_num'] = probe_i
+downsample_dt = 25  # sample period in ms
+# %%
+#----------------------------------------------------------------------#
+# Align the spikes with behavior
+for probe_i in range(1, metadata['n_probes']+1):
+    metadata['probe_num'] = probe_i
     
-#     # load neural data: [number of neurons x time bins in ms]
-#     spike_mat = load(preprocess_dir + f"probe{probe_i}/" + spike_mat_str_indiv)['spike_mat']
-#     behav_df = load(preprocess_dir + 'behav_df')
+    # load neural data: [number of neurons x time bins in ms]
+    spike_mat = load(preprocess_dir + f"probe{probe_i}/" + spike_mat_str_indiv)['spike_mat']
+    behav_df = load(preprocess_dir + 'behav_df')
     
     
-#     # align spike times to behavioral data timeframe
-#     # spike_times_start_aligned = array [n_neurons x n_trials x longest_trial period in ms]
-#     trialwise_spike_mat_start_aligned, _ = trace_utils.trial_start_align(behav_df, spike_mat, metadata, sps=1000)
+    # align spike times to behavioral data timeframe
+    # spike_times_start_aligned = array [n_neurons x n_trials x longest_trial period in ms]
+    trialwise_spike_mat_start_aligned, _ = trace_utils.trial_start_align(behav_df, spike_mat, metadata, sps=1000)
     
-#     # subsample (bin) data:
-#     # [n_neurons x n_trials x (-1 means numpy calculates: trial_len / dt) x ds]
-#     # then sum over the dt bins
-#     n_neurons = trialwise_spike_mat_start_aligned.shape[0]
-#     n_trials = trialwise_spike_mat_start_aligned.shape[1]
-#     trial_binned_mat_start_align = trialwise_spike_mat_start_aligned.reshape(n_neurons, n_trials, -1, downsample_dt)
-#     trial_binned_mat_start_align = trial_binned_mat_start_align.sum(axis=-1)  # sum over bins
+    # subsample (bin) data:
+    # [n_neurons x n_trials x (-1 means numpy calculates: trial_len / dt) x ds]
+    # then sum over the dt bins
+    n_neurons = trialwise_spike_mat_start_aligned.shape[0]
+    n_trials = trialwise_spike_mat_start_aligned.shape[1]
+    trial_binned_mat_start_align = trialwise_spike_mat_start_aligned.reshape(n_neurons, n_trials, -1, downsample_dt)
+    trial_binned_mat_start_align = trial_binned_mat_start_align.sum(axis=-1)  # sum over bins
     
-#     results = {'binned_mat': trial_binned_mat_start_align, 'downsample_dt': downsample_dt}
-#     dump(results, preprocess_dir + f"probe{metadata['probe_num']}/trial_binned_mat_start_align.npy", compress=3)
+    results = {'binned_mat': trial_binned_mat_start_align, 'downsample_dt': downsample_dt}
+    dump(results, preprocess_dir + f"probe{metadata['probe_num']}/trial_binned_mat_start_align.npy", compress=3)
